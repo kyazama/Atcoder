6a921ccdc87cdd0f7b8ecba0c5839b18b6287691
KotaroKazama0624 <ec27096h@gmail.com>
2025-05-09 21:57:16 +0900

    abc312-aを解いた

---
+++ workspace/20250509_ABC312-A.py
@@ -0,0 +1,18 @@
+s = input()
+
+if s == "ACE":
+    print("Yes")
+elif s == "BDF":
+    print("Yes")
+elif s == "CEG":
+    print("Yes")
+elif s == "DFA":
+    print("Yes")
+elif s == "EGB":
+    print("Yes")
+elif s == "FAC":
+    print("Yes")
+elif s == "GBD":
+    print("Yes")
+else:
+    print("No")
