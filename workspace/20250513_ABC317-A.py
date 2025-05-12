4ee3c3664023a875ea9279297dc66efaaff14f33
KotaroKazama0624 <ec27096h@gmail.com>
2025-05-13 07:40:39 +0900

    コメントを追加

--- workspace/20250513_ABC317-A.py
+++ workspace/20250513_ABC317-A.py
@@ -7,3 +7,7 @@ for i in range(n):
     if p[i] >= target_hitpoint:
         print(i + 1)
         break
+
+# if p[i] >= target_hitpoint: こうじゃなくて
+# p[i]+H >= x: このほうが変数名一つ減って良さそう
+# でも，web開発的にはわかりやすい変数名があった方が良いと思っている
